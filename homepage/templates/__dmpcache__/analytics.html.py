94d621fcaecf076ff8c0dc6be760d133640cac6d
mcmtrnt <mcmtrnt3@gmail.com>
2019-04-11 12:00:34 -0600

    commit

--- homepage/templates/__dmpcache__/analytics.html.py
+++ homepage/templates/__dmpcache__/analytics.html.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554939117.1513238
+_modified_time = 1555003013.4600227
 _enable_loop = True
 _template_filename = 'C:/Users/Trent/intex/homepage/templates/analytics.html'
 _template_uri = 'analytics.html'
@@ -30,9 +30,9 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        request = context.get('request', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
+        request = context.get('request', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
@@ -47,13 +47,13 @@ def render_body(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        request = context.get('request', UNDEFINED)
         def content():
             return render_content(context)
+        request = context.get('request', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n')
         if request.user.has_perm('auth.view_analytics'):
-            __M_writer('    <div class="container-fluid">\r\n        <div class="row content">\r\n            <div class="col-sm-3">\r\n                <h4>Available Analysis:</h4>\r\n                <ul>\r\n                    <li><a href=\'/homepage/highestOpioidPrescribers/\'>Highest Opioid Prescribers</a></li>\r\n                    <li><a href=\'/homepage/amIHighRisk/\'>Am I High Risk</a></li>\r\n')
+            __M_writer('    <div class="container-fluid">\r\n        <div class="row content">\r\n            <div class="col-sm-3">\r\n                <h4>Available Analysis:</h4>\r\n                <ul>\r\n                    <li><a href=\'/homepage/highestOpioidPrescribers/\'>Highest Opioid Prescribers</a></li>\r\n                    <li><a href=\'/homepage/opioidVisualization/\'>Opioid/Prescriptions Visualization</a></li>\r\n                    <li><a href=\'/homepage/amIHighRisk/\'>Am I High Risk</a></li>\r\n')
             if request.user.has_perm('homepage.view_doctor'):
                 __M_writer("                        <li><a href='/homepage/similarPrescribers/'>Find Similar Prescribers</a></li>\r\n                        <li><a href='/homepage/visualizations/'>Individual Visualizations</a></li>\r\n")
             __M_writer('                    <li><a href=\'/homepage/maps/\'>Map of Overall Prescriptions/Opiods</a></li>\r\n                    <li><a href=\'/homepage/mapDeaths/\'>Map of Opioid Related Deaths</a></li>\r\n                    <li><a href=\'/homepage/specialtyComparator/\'>Specialty Comparator</a></li>\r\n                    <li><a href=\'/homepage/specialtyVisualization/\'>Specialty Visualization</a></li>\r\n                    <li><a href=\'/homepage/prescribersVisualization/\'>Prescribers Visualization</a></li>\r\n                </ul>\r\n        \r\n            </div>\r\n        \r\n            <div class="col-sm-9">\r\n\r\n                <h4>Use the options on the left to view analytics and visualizations regarding opioid prescriptions</h4>\r\n\r\n            </div>\r\n\r\n        </div>\r\n    </div>\r\n')
@@ -67,6 +67,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Trent/intex/homepage/templates/analytics.html", "uri": "analytics.html", "source_encoding": "utf-8", "line_map": {"29": 0, "37": 1, "47": 3, "54": 3, "55": 4, "56": 5, "57": 12, "58": 13, "59": 16, "60": 33, "61": 34, "62": 36, "68": 62}}
+{"filename": "C:/Users/Trent/intex/homepage/templates/analytics.html", "uri": "analytics.html", "source_encoding": "utf-8", "line_map": {"29": 0, "37": 1, "47": 3, "54": 3, "55": 4, "56": 5, "57": 13, "58": 14, "59": 17, "60": 34, "61": 35, "62": 37, "68": 62}}
 __M_END_METADATA
 """
--- homepage/templates/__dmpcache__/base.htm.py
+++ homepage/templates/__dmpcache__/base.htm.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554999772.703655
+_modified_time = 1555005576.9637246
 _enable_loop = True
 _template_filename = 'C:/Users/Trent/intex/homepage/templates/base.htm'
 _template_uri = 'base.htm'
@@ -19,16 +19,16 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        request = context.get('request', UNDEFINED)
         self = context.get('self', UNDEFINED)
+        request = context.get('request', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
         __M_writer = context.writer()
-        __M_writer('<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n\r\n        <title>Opioid Stats</title>\r\n        <link rel="icon" href="/static/homepage/media/opioid.ico/">\r\n\r\n')
+        __M_writer('<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n\r\n        <title>OpioInfo</title>\r\n        <link rel="icon" href="/static/homepage/media/3b375d48-0186-450c-aa3c-e7bbf013b0de.png/">\r\n\r\n')
         __M_writer('        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\r\n\r\n        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">\r\n        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>\r\n\r\n\r\n        \r\n        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\r\n\r\n')
         __M_writer('        <script src="/django_mako_plus/dmp-common.min.js"></script>\r\n        ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( django_mako_plus.links(self) ))
-        __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n\r\n        <header>\r\n            <div class="nav navbar-default custom-nav justify-content-between">               \r\n                <h1>Opioid Stats</h1>\r\n                \r\n                    <li class="nav-item active">\r\n                        <a class="btn btn-outline-primary" href="/homepage/index/">Home <span class="sr-only">(current)</span></a>\r\n                    </li>\r\n')
+        __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n\r\n        <header>\r\n            <div class="nav navbar-default custom-nav justify-content-between">\r\n                <div>            \r\n                    <img style="width: 4rem; height: 4rem;" src="/static/homepage/media/3b375d48-0186-450c-aa3c-e7bbf013b0de.png/"/>\r\n                </div>   \r\n                    <li class="nav-item active">\r\n                        <a class="btn btn-outline-primary" href="/homepage/index/">Home <span class="sr-only">(current)</span></a>\r\n                    </li>\r\n')
         if request.user.is_authenticated:
             __M_writer('                        <li class="nav-item">\r\n                            <a class="btn btn-outline-primary" href="/homepage/main/">Main Page</a>\r\n                        </li>\r\n')
             if request.user.has_perm('auth.change_user'):
@@ -67,6 +67,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Trent/intex/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "27": 2, "28": 11, "29": 21, "30": 22, "31": 22, "32": 34, "33": 35, "34": 38, "35": 39, "36": 43, "37": 44, "38": 48, "39": 49, "40": 56, "41": 57, "42": 58, "43": 61, "44": 62, "45": 66, "50": 72, "56": 70, "62": 70, "68": 62}}
+{"filename": "C:/Users/Trent/intex/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "27": 2, "28": 11, "29": 21, "30": 22, "31": 22, "32": 35, "33": 36, "34": 39, "35": 40, "36": 44, "37": 45, "38": 49, "39": 50, "40": 57, "41": 58, "42": 59, "43": 62, "44": 63, "45": 67, "50": 73, "56": 71, "62": 71, "68": 62}}
 __M_END_METADATA
 """
