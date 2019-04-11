2c5618ef06ccc7ad8c0bf2e5a9fab16efc6a355d
mcmtrnt <mcmtrnt3@gmail.com>
2019-04-11 10:09:35 -0600

    works

--- homepage/views/prescriberDetails.py
+++ homepage/views/prescriberDetails.py
@@ -84,6 +84,8 @@ def related(request, param):
 
     response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
 
+    print(response.text)
+
     start = str(response.text).find('Values":[["')
     s = str(response.text)[start:]            
     end = str(response.text).find(']]')
