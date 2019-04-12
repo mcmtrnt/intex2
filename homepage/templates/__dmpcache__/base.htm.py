a80d6cdee3355a7977945417bc23aed2169cbfc6
mcmtrnt <mcmtrnt3@gmail.com>
2019-04-11 19:09:20 -0600

    logo

--- homepage/templates/__dmpcache__/base.htm.py
+++ homepage/templates/__dmpcache__/base.htm.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1555030974.3128633
+_modified_time = 1555031342.491766
 _enable_loop = True
 _template_filename = 'C:/Users/Trent/intex/homepage/templates/base.htm'
 _template_uri = 'base.htm'
@@ -27,13 +27,11 @@ def render_body(context,**pageargs):
         __M_writer = context.writer()
         __M_writer('<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n\r\n        <title>OpioInfo</title>\r\n        <link rel="icon" href="')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
-        __M_writer('homepage/media/3b375d48-0186-450c-aa3c-e7bbf013b0de.png/">\r\n        \r\n\r\n')
+        __M_writer('homepage/media/logo.png/">\r\n        \r\n\r\n')
         __M_writer('        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\r\n\r\n        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">\r\n        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>\r\n\r\n\r\n        \r\n        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\r\n\r\n')
         __M_writer('        <script src="/django_mako_plus/dmp-common.min.js"></script>\r\n        ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( django_mako_plus.links(self) ))
-        __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n\r\n        <header>\r\n            <div class="nav navbar-default custom-nav justify-content-between">\r\n                <div>\r\n                    <img style="width: 4rem; height: 4rem;" src="')
-        __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
-        __M_writer('homepage/media/3b375d48-0186-450c-aa3c-e7bbf013b0de.png/"/>\r\n                </div>\r\n                     \r\n                    <li class="nav-item active">\r\n                        <a class="btn btn-outline-primary" href="/homepage/index/">Home <span class="sr-only">(current)</span></a>\r\n                    </li>\r\n')
+        __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n\r\n        <header>\r\n            <div class="nav navbar-default custom-nav justify-content-between">\r\n                <h1>OpioInfo</h1>\r\n                     \r\n                    <li class="nav-item active">\r\n                        <a class="btn btn-outline-primary" href="/homepage/index/">Home <span class="sr-only">(current)</span></a>\r\n                    </li>\r\n')
         if request.user.is_authenticated:
             __M_writer('                        <li class="nav-item">\r\n                            <a class="btn btn-outline-primary" href="/homepage/main/">Main Page</a>\r\n                        </li>\r\n')
             if request.user.has_perm('auth.change_user'):
@@ -72,6 +70,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Trent/intex/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "28": 2, "29": 8, "30": 8, "31": 12, "32": 22, "33": 23, "34": 23, "35": 31, "36": 31, "37": 37, "38": 38, "39": 41, "40": 42, "41": 46, "42": 47, "43": 51, "44": 52, "45": 59, "46": 60, "47": 61, "48": 64, "49": 65, "50": 69, "55": 75, "61": 73, "67": 73, "73": 67}}
+{"filename": "C:/Users/Trent/intex/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "28": 2, "29": 8, "30": 8, "31": 12, "32": 22, "33": 23, "34": 23, "35": 35, "36": 36, "37": 39, "38": 40, "39": 44, "40": 45, "41": 49, "42": 50, "43": 57, "44": 58, "45": 59, "46": 62, "47": 63, "48": 67, "53": 73, "59": 71, "65": 71, "71": 65}}
 __M_END_METADATA
 """
